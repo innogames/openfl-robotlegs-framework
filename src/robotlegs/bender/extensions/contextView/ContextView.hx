bd6eeccf69361ca49e537ddc8d33a7df91d5c30c
oleg.reva <oleg.reva@innogames.com>
2018-02-16 14:57:37 +0100

    Small refactoring of ExtensionInstaller

--- src/robotlegs/bender/extensions/contextView/ContextView.hx
+++ src/robotlegs/bender/extensions/contextView/ContextView.hx
@@ -17,24 +17,7 @@ import openfl.display.DisplayObjectContainer;
 @:rtti
 class ContextView
 {
-
-	/*============================================================================*/
-	/* Public Properties                                                          */
-	/*============================================================================*/
-
 	public var view:DisplayObjectContainer;
-	//public var view(get, null):DisplayObjectContainer;
-	/**
-	 * The root DisplayObjectContainer for this Context
-	 */
-	//public function get_view():DisplayObjectContainer
-	//{
-		//return _view;
-	//}
-
-	/*============================================================================*/
-	/* Constructor                                                                */
-	/*============================================================================*/
 
 	/**
 	 * The Context View represents the root DisplayObjectContainer for a Context
